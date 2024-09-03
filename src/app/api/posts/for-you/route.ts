0e7a77a95051634918be3ada7578dc9c08475027
BruceLuo <295448382@qq.com>
2024-09-03 16:36:31 +0800

    feat: 使用useInfiniteQuery改造首页帖子无限加载

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,12 +1,15 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { postDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
 
-/**
- * 获取首页帖子
- */
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    // 获取下一页的游标
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    // 每页的帖子数量
+    const pageSize = 10;
+
     const { user } = await validateRequest();
 
     if (!user) {
@@ -16,9 +19,19 @@ export async function GET() {
     const posts = await prisma.post.findMany({
       include: postDataInclude,
       orderBy: { createdAt: "desc" },
+      take: pageSize + 1, // 获取多一个以便判断是否有下一页
+      cursor: cursor ? { id: cursor } : undefined, // 游标分页，从 cursor 开始查询
     });
 
-    return Response.json(posts);
+    // 如果帖子数量大于 pageSize，说明有下一页
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize), // 只获取 pageSize 个帖子
+      nextCursor, // 下一页的游标
+    };
+
+    return Response.json(data);
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
