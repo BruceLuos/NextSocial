e345a33eea69433e27d17623b751ace0a19e5fef
BruceLuo <295448382@qq.com>
2024-09-12 16:15:22 +0800

    feat: uoloadthing 上传暂存

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
     }
   },
   serverExternalPackages: ["@node-rs/argon2"], // 指定服务器组件所需的外部包，lucia-auth中使用了argon2进行密码hash处理a，需要指定
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,51 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { createUploadthing, FileRouter } from "uploadthing/next";
+import { UploadThingError, UTApi } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const fileRouter = {
+  avatar: f({
+    image: { maxFileSize: "512KB" },
+  })
+    .middleware(async () => {
+      // 验证是否登录
+      const { user } = await validateRequest();
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { user };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // 文件上传成功时metadata会包含user信息
+      const oldAvatarUrl = metadata.user.avatarUrl;
+
+      // 删除旧的文件
+      if (oldAvatarUrl) {
+        const key = oldAvatarUrl.split(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+        )[1];
+
+        await new UTApi().deleteFiles(key);
+      }
+
+      // 更新新的文件
+      const newAvatarUrl = file.url.replace(
+        "/f/",
+        `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+      );
+
+      // 更新用户数据
+      await prisma.user.update({
+        where: { id: metadata.user.id },
+        data: {
+          avatarUrl: newAvatarUrl,
+        },
+      });
+
+      return { avatarUrl: newAvatarUrl };
+    }),
+} satisfies FileRouter;
+
+export type AppFileRouter = typeof fileRouter;
