8a38041d8ba744f76523c4fea3e090e5df634ce0
BruceLuo <295448382@qq.com>
2024-09-15 23:57:25 +0800

    feat: 用户帖子action补充提交附件

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -5,17 +5,24 @@ import prisma from "@/lib/prisma";
 import { getPostDataInclude } from "@/lib/types";
 import { createPostSchema } from "@/lib/validation";
 
-export async function submitPost(input: string) {
+export async function submitPost(input: {
+  content: string;
+  mediaIds: string[];
+}) {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { content } = createPostSchema.parse({ content: input });
+  const { content, mediaIds } = createPostSchema.parse({ content: input });
 
   const newPost = await prisma.post.create({
     data: {
       content,
       userId: user.id,
+      // 附件
+      attachments: {
+        connect: mediaIds.map((id) => ({ id })),
+      },
     },
     include: getPostDataInclude(user.id), // 帖子创建时包含用户信息
   });
