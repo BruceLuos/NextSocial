0337e9e12bb9214c4b63b2f0a1bdd42dff23a103
BruceLuo <295448382@qq.com>
2024-10-06 16:23:50 +0800

    feat: stream 聊天部分设置

---
+++ src/app/api/get-token/route.ts
@@ -0,0 +1,29 @@
+import { validateRequest } from "@/auth";
+import streamServerClient from "@/lib/stream";
+
+export async function GET() {
+  try {
+    const { user } = await validateRequest();
+
+    console.log("Calling get-token for user: ", user?.id);
+
+    if (!user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60; // 包含当前时间加上1小时后的时间戳，表示令牌的过期时间。
+
+    const issuedAt = Math.floor(Date.now() / 1000) - 60; // 包含当前时间减去1分钟后的时间戳，表示令牌的签发时间。
+
+    const token = streamServerClient.createToken(
+      user.id,
+      expirationTime,
+      issuedAt,
+    );
+
+    return Response.json({ token });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
