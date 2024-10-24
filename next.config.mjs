4c31fd7a8afaff3fffb00ab4e6fcd20768d6781d
BruceLuo <295448382@qq.com>
2024-10-24 23:19:02 +0800

    feat: 帖子内容hastag点击重定向到搜索页

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     staleTimes: {
       dynamic: 30, // nextjs15以前页面默认会缓存，现在nextjs15需要配置才会启动缓存，这里设置30秒缓存，减少tag数据多次请求
-    }
+    },
   },
   serverExternalPackages: ["@node-rs/argon2"], // 指定服务器组件所需的外部包，lucia-auth中使用了argon2进行密码hash处理a，需要指定
   images: {
@@ -14,6 +14,15 @@ const nextConfig = {
         pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
       },
     ],
+  },
+  rewrites: () => {
+    return [
+      // 点击帖子#tag重定向到搜索页
+      {
+        source: "/hashtag/:tag",
+        destination: "/search?q=%23:tag",
+      },
+    ];
   },
 };
 
