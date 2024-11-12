1475cf92e404e4a8fa5fe4d2c0d9ef701aafcadf
BruceLuo <295448382@qq.com>
2024-11-12 22:23:03 +0800

    docs: some

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # NextSocial
-This is the starting code for this tutorial.
 
 **IMPORTANT**: After cloning the repo, open the command line inside the project and run `npm i --legacy-peer-deps`.
 
--- src/hooks/useFollowerInfo.ts
+++ src/hooks/useFollowerInfo.ts
@@ -2,6 +2,7 @@ import kyInstance from "@/lib/ky";
 import { FollowerInfo } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 
+/** 获取关注者数据 */
 export default function useFollowerInfo(
   userId: string,
   initialState: FollowerInfo,
