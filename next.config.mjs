83cd211a6b71c502b4f225dc8c690729aa1cef60
BruceLuo <295448382@qq.com>
2025-01-09 11:01:18 +0800

    docs: 修改staleTimes备注

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // 该配置主要影响 Dynamic Functions（如动态路由）的缓存行为
+    // 在 Next.js 15 中，默认禁用了动态路由的缓存
+    // 设置 staleTimes 后会启用缓存
     staleTimes: {
       dynamic: 30, // nextjs15以前页面默认会缓存，现在nextjs15需要配置才会启动缓存，这里设置30秒缓存，减少tag数据多次请求
     },
